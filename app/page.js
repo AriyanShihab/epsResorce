8bad0e3abe2573eb391bafe19df1da0fe824429f
Ariyan Shihab <devshihab111@gmail.com>
2024-06-22 20:16:47 +0600

    add something

---
+++ app/page.js
@@ -0,0 +1,14 @@
+// import { Main } from "next/document";
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+  <div>
+<h1 className="text-center py-5">
+  HEllo World
+</h1>
+  </div>
+  );
+}
+
